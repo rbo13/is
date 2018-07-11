a709c6943e2116c0e01c4f25799708f3083d8424
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-07-11 22:19:22 +0800

    Refactored to add new single method to assert
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- is.go
+++ is.go
@@ -40,21 +40,30 @@ func (i *Is) NotNil(val interface{}) bool {
 
 // Int64 evaluates val if of type int64
 func (i *Is) Int64(val interface{}) {
-	if reflect.TypeOf(val).Kind() != reflect.Int64 {
-		i.testing.Errorf("Expecting as type int64, but got: %v", reflect.TypeOf(val).Kind())
-	}
+	i.assertVal(val, reflect.Int64)
+	// if reflect.TypeOf(val).Kind() != reflect.Int64 {
+	// 	i.testing.Errorf("Expecting as type int64, but got: %v", reflect.TypeOf(val).Kind())
+	// }
 }
 
 // Int32 evaluates val if of type int32
 func (i *Is) Int32(val interface{}) {
-	if reflect.TypeOf(val).Kind() != reflect.Int32 {
-		i.testing.Errorf("Expecting as type int32, but got: %v", reflect.TypeOf(val).Kind())
-	}
+	i.assertVal(val, reflect.Int32)
+	// if reflect.TypeOf(val).Kind() != reflect.Int32 {
+	// 	i.testing.Errorf("Expecting as type int32, but got: %v", reflect.TypeOf(val).Kind())
+	// }
 }
 
 // String evalueates val if of type string
 func (i *Is) String(val interface{}) {
-	if reflect.TypeOf(val).Kind() != reflect.String {
-		i.testing.Errorf("Expecting as type string, but got: %v", reflect.TypeOf(val).Kind())
+	i.assertVal(val, reflect.String)
+	// if reflect.TypeOf(val).Kind() != reflect.String {
+	// 	i.testing.Errorf("Expecting as type string, but got: %v", reflect.TypeOf(val).Kind())
+	// }
+}
+
+func (i *Is) assertVal(val interface{}, kind reflect.Kind) {
+	if reflect.TypeOf(val).Kind() != kind {
+		i.testing.Errorf("AssertionError: expected as type %v, but got: %v instead", kind, reflect.TypeOf(val).Kind())
 	}
 }
